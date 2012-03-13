74d636699784e1466496051fce5ac7c964a8cc43
Sakti Dwi Cahyono <54krpl@gmail.com>
2012-03-13 07:43:28 +0700

    setup for distributing package

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+install_requires = [
+    'matplotlib'
+]
+
+
+setup(
+    name='gperf',
+    version='0.1dev',
+    license='BSD',
+    author='Sakti Dwi Cahyono',
+    author_email='54krpl at gmail dot com',
+    keywords='sar sysstat time-based graph',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion'
+        ],
+    entry_points={'console_scripts':['gperf = gperf.gperf:main']},
+    description='Graphing from sysstat sar data into time-based graph',
+    long_description=open('README.rst').read(),
+    url='https://github.com/sakti/gperf',
+)
